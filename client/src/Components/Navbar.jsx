0e9db6809ed34c4cf95911f9a98d91120cfccaca
ShubhChaturvedi <chaturvedishubh650@gmail.com>
2023-08-05 07:58:16 +0530

    Dev: Initial

---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand fw-bold" href="#">
+      <img src="https://cdn-icons-png.flaticon.com/512/3845/3845808.png" alt="Logo" width="30" height="24" className="d-inline-block align-text-top mx-3"/>
+      Keacilius meet
+    </a>
+  </div>
+</nav>
+    </div>
+  )
+}
